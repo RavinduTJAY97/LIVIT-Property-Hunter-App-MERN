b9caf696f54e61db900a1c69240190aca7c070d4
Ravindu Jayasingha <ravindu.jayasingha@arimaclanka.com>
2024-06-15 14:12:48 +0530

    User and Property DB models created

---
+++ server/database/models/property.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const propertySchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  location: { type: String, required: true },
+  numberOfBathrooms: { type: Number, required: true },
+  numberOfBedrooms: { type: Number, required: true },
+  numberOfParkings: { type: Number, required: true },
+  description: { type: String, required: true },
+  propertyType: { type: String, required: true },
+  status: { type: String, required: true },
+  mainImageUrl: { type: String, required: true },
+  imageUrls: { type: [String], required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+const Property = mongoose.model("Property", propertySchema);
+module.exports = Property;
