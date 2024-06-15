2d3de029d28300c91c01076579c6c75b185b75ef
Ravindu Jayasingha <ravindu.jayasingha@arimaclanka.com>
2024-06-15 14:22:52 +0530

    Property routes added in Index

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const connectDB = require("./database/connect");
 const app = express();
+const bodyParser = require("body-parser");
 require("dotenv").config();
+const propertyRoutes = require("./routes/propertyRoutes");
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 // Connect to MongoDB
 connectDB(process.env.MONGODB_CONNECTION);
@@ -13,3 +15,6 @@ const port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
+
+// property routes
+app.use("/property", propertyRoutes);
