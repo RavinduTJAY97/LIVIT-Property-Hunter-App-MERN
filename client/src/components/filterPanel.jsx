41a914b8f49b3274dad169cfae83b3050a9588ee
Ravindu Jayasingha <ravindu.jayasingha@arimaclanka.com>
2024-06-17 23:27:40 +0530

    filterpanel component updated for search and filter functions

--- client/src/components/filterPanel.jsx
+++ client/src/components/filterPanel.jsx
@@ -8,25 +8,27 @@ import {
   MenuItem,
 } from "@mui/material";
 
-const FilterPanel = () => {
+const FilterPanel = ({ emitter }) => {
   const [filter1, setFilter1] = React.useState("");
   const [filter2, setFilter2] = React.useState("");
   const [searchTerm, setSearchTerm] = React.useState("");
 
-  const handleFilter1Change = (event) => {
+  const handlePropertyTypeChange = (event) => {
     setFilter1(event.target.value);
+    emitter.emit("propertyType", event.target.value);
   };
 
-  const handleFilter2Change = (event) => {
+  const handleStatusChange = (event) => {
     setFilter2(event.target.value);
+    emitter.emit("status", event.target.value);
   };
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
+    emitter.emit("search", event.target.value);
   };
 
   return (
-    // < -- Filter panel  -->
     <Box
       sx={{
         width: 250,
@@ -39,7 +41,6 @@ const FilterPanel = () => {
         height: "100%",
       }}
     >
-      {/* Search field */}
       <TextField
         label="Search"
         variant="outlined"
@@ -49,51 +50,42 @@ const FilterPanel = () => {
           style: { color: "#37517e" },
         }}
       />
-      {/* /Search field */}
-
-      {/* Filter by location */}
       <FormControl fullWidth>
         <InputLabel id="filter1-label" style={{ color: "#37517e" }}>
-          Filter 1
+          Property Type
         </InputLabel>
         <Select
           labelId="filter1-label"
           value={filter1}
-          onChange={handleFilter1Change}
+          onChange={handlePropertyTypeChange}
           label="Filter 1"
         >
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={10}>Option 1</MenuItem>
-          <MenuItem value={20}>Option 2</MenuItem>
-          <MenuItem value={30}>Option 3</MenuItem>
+          <MenuItem value="House">House</MenuItem>
+          <MenuItem value="Apartment">Apartment</MenuItem>
+          <MenuItem value="Condo">Condo</MenuItem>
         </Select>
       </FormControl>
-      {/* /Filter by location */}
-
-      {/* Filter by property type */}
       <FormControl fullWidth>
         <InputLabel id="filter2-label" style={{ color: "#37517e" }}>
-          Filter 2
+          Status
         </InputLabel>
         <Select
           labelId="filter2-label"
           value={filter2}
-          onChange={handleFilter2Change}
+          onChange={handleStatusChange}
           label="Filter 2"
         >
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={10}>Option A</MenuItem>
-          <MenuItem value={20}>Option B</MenuItem>
-          <MenuItem value={30}>Option C</MenuItem>
+          <MenuItem value="AVAILABLE">Available</MenuItem>
+          <MenuItem value="SOLD">Sold</MenuItem>
+          <MenuItem value="PENDING">Pending</MenuItem>
         </Select>
       </FormControl>
-      {/* /Filter by property type */}
-
-      {/* logo */}
       <Box
         display="flex"
         alignItems="center"
@@ -106,9 +98,7 @@ const FilterPanel = () => {
           style={{ maxHeight: "150px" }}
         />
       </Box>
-      {/* /logo */}
     </Box>
-    // </ -- Filter panel -->
   );
 };
 
