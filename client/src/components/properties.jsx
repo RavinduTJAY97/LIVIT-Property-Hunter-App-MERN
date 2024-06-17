d8b7265c1cae0fec87befaf6084aa681ad8ec54b
Ravindu Jayasingha <ravindu.jayasingha@arimaclanka.com>
2024-06-17 23:28:36 +0530

    properties component updated for search and filter functions

--- client/src/components/properties.jsx
+++ client/src/components/properties.jsx
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FilterPanel from "./filterPanel";
 import PropertyList from "./propertyList";
 import { Grid, Box } from "@mui/material";
+import EventEmitter from "eventemitter3";
+
+const emitter = new EventEmitter();
 
 const Properties = () => {
+  const [propertyType, setPropertyType] = useState("");
+  const [status, setStatus] = useState("");
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const handlePropertyTypeChange = (type) => {
+      setPropertyType(type);
+    };
+    const handleStatusChange = (status) => {
+      setStatus(status);
+    };
+    const handleSearch = (search) => {
+      setSearch(search);
+    };
+
+    emitter.on("propertyType", handlePropertyTypeChange);
+    emitter.on("status", handleStatusChange);
+    emitter.on("search", handleSearch);
+
+    return () => {
+      emitter.off("propertyType", handlePropertyTypeChange);
+      emitter.off("status", handleStatusChange);
+      emitter.off("search", handleSearch);
+    };
+  }, []);
+
   return (
     <div>
       <Box sx={{ display: "flex", width: "100%" }}>
         <Grid container spacing={2} padding={5}>
-          {/* Filter panel */}
           <Grid item xs={3}>
-            <FilterPanel />
+            <FilterPanel emitter={emitter} />
           </Grid>
-          {/* /Filter panel */}
-          {/* Property list */}
           <Grid item xs={9}>
-            <PropertyList />
+            <PropertyList
+              propertyType={propertyType}
+              status={status}
+              search={search}
+            />
           </Grid>
-          {/* /Property list */}
         </Grid>
       </Box>
     </div>
