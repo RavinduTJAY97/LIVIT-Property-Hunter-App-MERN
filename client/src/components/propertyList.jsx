447447852f92c526ddb95031964c06a77def38e5
Ravindu Jayasingha <ravindu.jayasingha@arimaclanka.com>
2024-06-17 23:29:06 +0530

    propertyList component updated for search and filter functions

--- client/src/components/propertyList.jsx
+++ client/src/components/propertyList.jsx
@@ -15,29 +15,38 @@ import BedIcon from "@mui/icons-material/Bed";
 import BathtubIcon from "@mui/icons-material/Bathtub";
 import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
 
-const ProductsList = () => {
+const PropertyList = ({ propertyType, status, search }) => {
   const [properties, setProperties] = useState([]);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-
   const propertiesPerPage = 4;
 
-  /* Fetching property data based on page */
   useEffect(() => {
-    axios
-      .get(
-        `http://localhost:8080/property?page=${page}&limit=${propertiesPerPage}`
-      )
-      .then((response) => {
+    const fetchProperties = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8080/property`, {
+          params: {
+            page,
+            limit: propertiesPerPage,
+            propertyType,
+            status,
+            search,
+          },
+        });
         setProperties(response.data.properties || []);
         setTotalPages(Math.ceil(response.data.total / propertiesPerPage));
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, [page]);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchProperties();
+  }, [page, propertyType, status, search]);
 
   const handleChange = (event, value) => {
     setPage(value);
   };
+
   const formatPrice = (price) => {
     return new Intl.NumberFormat("en-US").format(price);
   };
@@ -45,93 +54,88 @@ const ProductsList = () => {
   return (
     <Container>
       <Grid container spacing={3} direction="column">
-        {properties.map((property) => (
-          <Grid item xs={12} key={property._id}>
-            {/* Add product card  */}
-            <Card
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center",
-                padding: 2,
-                maxWidth: 900,
-                position: "relative",
-                boxShadow:
-                  "0px 2px 1px -1px #fe5c00, 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12)",
-              }}
-            >
-              {/* Add property image  */}
-              <CardMedia
-                component="img"
-                sx={{ width: 150, height: 150 }}
-                image={"assets/imgs/ap1_5.jpg"}
-                alt={property.title}
-              />
-              {/* /Add property image */}
-
-              {/* Add property details  */}
-              <CardContent sx={{ flex: "1 0 auto" }}>
-                <Typography variant="h6" component="div">
-                  {property.title}
-                </Typography>
-
-                <Typography variant="subtitle2" color="text.secondary">
-                  {property.propertyType}
-                </Typography>
-
-                <Typography
-                  variant="subtitle2"
-                  color="text.secondary"
-                  mt={2}
-                  sx={{ display: "flex", alignItems: "center" }}
-                >
-                  <BedIcon sx={{ fontSize: 16, mr: 0.5 }} />{" "}
-                  {property.numberOfBedrooms} |
-                  <BathtubIcon sx={{ fontSize: 16, mr: 0.5 }} />{" "}
-                  {property.numberOfBathrooms} |
-                  <DirectionsCarIcon sx={{ fontSize: 16, mr: 0.5 }} />{" "}
-                  {property.numberOfParkings}
-                </Typography>
-              </CardContent>
-              {/* /Add property details  */}
-
-              {/* Add property price  */}
-              <Box sx={{ position: "absolute", top: 75, right: 16 }}>
-                <Typography
-                  variant="h6"
-                  gutterBottom
-                  style={{ color: "#fe5c00" }}
-                >
-                  AUD {formatPrice(property.price)}
-                </Typography>
-              </Box>
-              {/* property location  */}
-              <Box
+        {properties.length === 0 ? (
+          <Grid item xs={12}>
+            <Typography variant="h6" align="center">
+              Sorry No properties available
+            </Typography>
+          </Grid>
+        ) : (
+          properties.map((property) => (
+            <Grid item xs={12} key={property._id}>
+              <Card
                 sx={{
-                  position: "absolute",
-                  top: 16,
-                  right: 16,
                   display: "flex",
+                  flexDirection: "row",
                   alignItems: "center",
+                  padding: 2,
+                  maxWidth: 900,
+                  position: "relative",
+                  boxShadow:
+                    "0px 2px 1px -1px #fe5c00, 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12)",
                 }}
               >
-                <LocationOnIcon
-                  sx={{ color: "#686D76", fontSize: 16, mr: 0.5 }}
+                <CardMedia
+                  component="img"
+                  sx={{ width: 150, height: 150 }}
+                  image={"assets/imgs/ap1_5.jpg"}
+                  alt={property.title}
                 />
-                <Typography
-                  variant="h6"
-                  gutterBottom
-                  style={{ color: "#686D76", fontSize: 12 }}
+                <CardContent sx={{ flex: "1 0 auto" }}>
+                  <Typography variant="h6" component="div">
+                    {property.title}
+                  </Typography>
+                  <Typography variant="subtitle2" color="text.secondary">
+                    {property.propertyType}
+                  </Typography>
+                  <Typography
+                    variant="subtitle2"
+                    color="text.secondary"
+                    mt={2}
+                    sx={{ display: "flex", alignItems: "center" }}
+                  >
+                    <BedIcon sx={{ fontSize: 16, mr: 0.5 }} />{" "}
+                    {property.numberOfBedrooms} |
+                    <BathtubIcon sx={{ fontSize: 16, mr: 0.5 }} />{" "}
+                    {property.numberOfBathrooms} |
+                    <DirectionsCarIcon sx={{ fontSize: 16, mr: 0.5 }} />{" "}
+                    {property.numberOfParkings}
+                  </Typography>
+                </CardContent>
+                <Box sx={{ position: "absolute", top: 75, right: 16 }}>
+                  <Typography
+                    variant="h6"
+                    gutterBottom
+                    style={{ color: "#fe5c00" }}
+                  >
+                    AUD {formatPrice(property.price)}
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    position: "absolute",
+                    top: 16,
+                    right: 16,
+                    display: "flex",
+                    alignItems: "center",
+                  }}
                 >
-                  {property.location}
-                </Typography>
-              </Box>
-            </Card>
-          </Grid>
-        ))}
+                  <LocationOnIcon
+                    sx={{ color: "#686D76", fontSize: 16, mr: 0.5 }}
+                  />
+                  <Typography
+                    variant="h6"
+                    gutterBottom
+                    style={{ color: "#686D76", fontSize: 12 }}
+                  >
+                    {property.location}
+                  </Typography>
+                </Box>
+              </Card>
+            </Grid>
+          ))
+        )}
       </Grid>
-
-      {/* Pagination  */}
       <Pagination
         count={totalPages}
         page={page}
@@ -147,9 +151,8 @@ const ProductsList = () => {
           },
         }}
       />
-      {/* /Pagination  */}
     </Container>
   );
 };
 
-export default ProductsList;
+export default PropertyList;
